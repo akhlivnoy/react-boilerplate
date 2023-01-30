f822f6e0dcc08cf1bf887f6cf86afc16469ddaf4
Andrii Khlivnyi <akhlivnoy@gmail.com>
2023-01-30 17:21:52 +0200

    babel & vite.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+
+import { useState } from "react";
+
+import reactLogo from "#assets/react.svg";
+import { HomePage } from "#pages";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <HomePage />
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img alt="Vite logo" className="logo" src="/vite.svg" />
+        <img alt="React logo" className="logo react" src={reactLogo} />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
---
+++ src/app/pages/Home/Home.tsx
@@ -0,0 +1,1 @@
+export const HomePage: React.ComponentType = () => <div>Home Page</div>;
